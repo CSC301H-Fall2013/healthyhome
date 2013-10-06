bc8c30f2e8f1b1237d34b6fa3340bbdb3226c78e
Shandheap <shandheap@gmail.com>
2013-10-06 16:21:45 -0400

    Fix syntax problem in settings.py.

--- DjangoApplication/DjangoApplication/settings.py
+++ DjangoApplication/DjangoApplication/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 ROOT_PATH = os.getcwd()
 
 # Check if the path given is Azure, if it is provide the folder path
-if !os.path.exists(ROOT_PATH + '/templates/complaint_system/'):
+if not os.path.exists(ROOT_PATH + '/templates/complaint_system/'):
     ROOT_PATH = 'D:/home/site/wwwroot/DjangoApplication/'
 
 DATABASES = {
