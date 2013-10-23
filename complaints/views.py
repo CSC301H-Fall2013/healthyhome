a4633a66a722ec24428412fc3fb1e284faf88b49
Shandheap <shandheap@gmail.com>
2013-10-23 01:05:33 -0400

    Add complaints folder back after fixing azure problem.

---
+++ complaints/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from complaints.models import Complaint
+#from complaints import ContactInfo
+from complaints.forms import AddComplaint
+
+
+class index(ListView):
+    model = Complaint
+
+
+class new(CreateView):
+    model = Complaint
+    form_class = AddComplaint
+	
+
+class update(UpdateView):
+    model = Complaint
+
+
+class delete(DeleteView):
+    model = Complaint
