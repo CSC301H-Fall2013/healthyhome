b8acc20eb6373829b9a3d726bbdc1a3e5ef3f1a6
Michael <michael.kozakov@gmail.com>
2013-11-30 00:57:44 -0500

    Fix marker hide/show logic

--- static/js/homepage-map.js
+++ static/js/homepage-map.js
@@ -11,12 +11,13 @@ function initialize() {
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var categoryMap = {};
+    var checkedCategories = {};
 
     // Get buildings from the API
-    $.getJSON('/api/v1/buildings', function(data) {onGetJSON(data, bounds, map, categoryMap)});
+    $.getJSON('/api/v1/buildings', function(data) {onGetJSON(data, bounds, map, categoryMap, checkedCategories)});
 }
 
-function onGetJSON(buildingList, bounds, map, categoryMap) {
+function onGetJSON(buildingList, bounds, map, categoryMap, checkedCategories) {
     //Place buildings on the map
     buildingList.forEach(function (building) {
         var latLong = new google.maps.LatLng(building.lat, building.lng);
@@ -26,6 +27,7 @@ function onGetJSON(buildingList, bounds, map, categoryMap) {
             map: map,
             title: building.title
         });
+        marker.categories = [];
 
         // set marker color base on the complaints number
         var complaintNum = building.categories.length;
@@ -41,11 +43,13 @@ function onGetJSON(buildingList, bounds, map, categoryMap) {
             window.location = "/building/" + building.id;
         });
         building.categories.forEach(function(category) {
+            marker.categories.push(category);
             if(categoryMap[category]) {
                 categoryMap[category].push(marker);
             } else {
                 categoryMap[category] = [marker];
             }
+            checkedCategories[category] = true;
         });
     });
     // Create the container for checkboxes
@@ -71,10 +75,25 @@ function onGetJSON(buildingList, bounds, map, categoryMap) {
         // Hide/show the appropriate markers
         checkbox.change(function() {
             var markers = categoryMap[category];
-            console.log(markers);
-            markers.forEach(function(marker) {
-                marker.setMap(checkbox.is(":checked") ? map : null);
-            });
+            if(checkbox.is(":checked")) {
+                markers.forEach(function(marker) {
+                    marker.setMap(map);
+                });
+                checkedCategories[category] = true;
+            } else {
+                checkedCategories[category] = false;
+                markers.forEach(function(marker) {
+                    var hide = true;
+                    marker.categories.forEach(function(c) {
+                        if(checkedCategories[c]) {
+                            hide = false;
+                        }
+                    });
+                    if(hide) {
+                        marker.setMap(null);
+                    }
+                });
+            }
         });
 
         label.append(checkbox, category);
